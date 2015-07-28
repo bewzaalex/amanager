1204d6746f48a5bf9425af2ee1e5e812355c59a6
bewza <bewza_alex@ukr.net>
2015-07-28 23:22:16 +0300

    Some CelSearchForm fixes

--- src/AppBundle/Form/CelSearchType.php
+++ src/AppBundle/Form/CelSearchType.php
@@ -15,37 +15,110 @@ class CelSearchType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('id', 'number', array('required' => false))
-            ->add('eventtype', 'text', array('required' => false))
+            ->add('id', 'number', array(
+                'required' => false,
+                'label' => 'ID: ',
+            ))
+            ->add('eventtype', 'text', array(
+                'required' => false,
+                'label' => 'Event Type: ',
+            ))
             // FIXME: set correct datetime type
             ->add('eventtime', 'datetime', array(
                 'date_format' => 'yyyy-MM-dd  HH:mm',
                 'format' => 'yyyy-MM-dd HH:mm',
                 'widget' => 'choice',
                 'required' => false,
+                'label' => 'Event Time: ',
             ))
-            ->add('cid_name', 'text', array('required' => false))
-            ->add('cid_num', 'text', array('required' => false))
-            ->add('cid_ani', 'text', array('required' => false))
-            ->add('cid_rdnis', 'text', array('required' => false))
-            ->add('cid_dnid', 'text', array('required' => false))
-            ->add('exten', 'text', array('required' => false))
-            ->add('context', 'text', array('required' => false))
-            ->add('channame', 'text', array('required' => false))
-            ->add('src', 'text', array('required' => false))
-            ->add('dst', 'text', array('required' => false))
-            ->add('channel', 'text', array('required' => false))
-            ->add('dstchannel', 'text', array('required' => false))
-            ->add('appname', 'text', array('required' => false))
-            ->add('appdata', 'text', array('required' => false))
-            ->add('amaflags', 'number', array('required' => false))
-            ->add('accountcode', 'text', array('required' => false))
-            ->add('uniqueid', 'text', array('required' => false))
-            ->add('linkedid', 'text', array('required' => false))
-            ->add('peer', 'text', array('required' => false))
-            ->add('userdeftype', 'text', array('required' => false))
-            ->add('eventextra', 'text', array('required' => false))
-            ->add('userfield', 'text', array('required' => false))
+            ->add('cid_name', 'text', array(
+                'required' => false,
+                'label' => 'CallerID Name: ',
+            ))
+            ->add('cid_num', 'text', array(
+                'required' => false,
+                'label' => 'CallerID Number: ',
+            ))
+            ->add('cid_ani', 'text', array(
+                'required' => false,
+                'label' => 'Cid_Ani: ',
+            ))
+            ->add('cid_rdnis', 'text', array(
+                'required' => false,
+                'label' => 'Cid_Rdnis: ',
+            ))
+            ->add('cid_dnid', 'text', array(
+                'required' => false,
+                'label' => 'Cid_Dnid: ',
+            ))
+            ->add('exten', 'text', array(
+                'required' => false,
+                'label' => 'Extension: ',
+            ))
+            ->add('context', 'text', array(
+                'required' => false,
+                'label' => 'Context: ',
+            ))
+            ->add('channame', 'text', array(
+                'required' => false,
+                'label' => 'Channel Name: ',
+            ))
+            ->add('src', 'text', array(
+                'required' => false,
+                'label' => 'Source: ',
+            ))
+            ->add('dst', 'text', array(
+                'required' => false,
+                'label' => 'Destination: ',
+            ))
+            ->add('channel', 'text', array(
+                'required' => false,
+                'label' => 'Channel: ',
+            ))
+            ->add('dstchannel', 'text', array(
+                'required' => false,
+                'label' => 'Destination Channel: ',
+            ))
+            ->add('appname', 'text', array(
+                'required' => false,
+                'label' => 'Application Name: ',
+            ))
+            ->add('appdata', 'text', array(
+                'required' => false,
+                'label' => 'Application Data: ',
+            ))
+            ->add('amaflags', 'number', array(
+                'required' => false,
+                'label' => 'Amaflags: ',
+            ))
+            ->add('accountcode', 'text', array(
+                'required' => false,
+                'label' => 'Accountcode: ',
+            ))
+            ->add('uniqueid', 'text', array(
+                'required' => false,
+                'label' => 'UniqueID: ',
+            ))
+            ->add('linkedid', 'text', array(
+                'required' => false,
+                'label' => 'LinkedID: ',
+            ))
+            ->add('peer', 'text', array(
+                'required' => false,
+                'label' => 'Peer: ',
+            ))
+            ->add('userdeftype', 'text', array(
+                'required' => false,
+                'label' => 'Userdeftype: ',
+            ))
+            ->add('eventextra', 'text', array(
+                'required' => false,
+                'label' => 'Eventextra: ',
+            ))
+            ->add('userfield', 'text', array(
+                'required' => false,
+                'label' => 'Userfield: ',
+            ))
         ;
     }
     
